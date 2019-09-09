f08f5ce51e2b448ffa206c0a348ee6744bdbe34d
Marco Iannuzzi <marco.iannuzzi@1ststop.co.uk>
2019-09-09 10:30:58 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-    <v-app>
+    <v-app id="app">
+        <app-header></app-header>
         <v-content></v-content>
     </v-app>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
     name: "App",
-    components: {},
-    data: () => ({
-        //
-    })
+    components: {
+        "app-header": Header
+    },
+    data: () => ({})
 };
 </script>
+
+<style>
+#app {
+    max-width: 1000px;
+    margin: auto;
+    padding: 50px;
+}
+</style>
