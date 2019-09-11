211465c3dcf4b2d5333d3e98b9abab31d96ab7cd
Marco Iannuzzi <marco.iannuzzi@1ststop.co.uk>
2019-09-11 10:17:01 +0100

    add post article form

--- src/pages/PostArticle.vue
+++ src/pages/PostArticle.vue
@@ -1,9 +1,28 @@
 <template>
-    <h2>post article</h2>
+    <div>
+        <h2>New Article</h2>
+        <form v-on:submit.prevent="postArticle">
+            <v-text-field v-model="title" color="red" placeholder="Title" required></v-text-field>
+            <v-textarea v-model="body" color="red" name="input-7-4" placeholder="Article" required></v-textarea>
+            <v-btn class="ma-2" tile outlined color="red" type="submit">
+                <v-icon left>mdi-pencil</v-icon>Post Article
+            </v-btn>
+        </form>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            title: "",
+            body: ""
+        };
+    },
+    methods: {
+        postArticle: function() {}
+    }
+};
 </script>
 
 <style>
